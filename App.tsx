8542ff7d5082587a39fc01173e54cfeedbb56936
vaibhav164 <vai1641996@gmail.com>
2025-06-24 02:23:09 +0530

    Updated App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import HomeScreen from './src/Screens/HomeScreen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -14,7 +7,7 @@ function App() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <HomeScreen />
     </View>
   );
 }
